1dfe45128a90d6794303d5c18d7357ca543e416a
김건형 <show11837@gmail.com>
2020-11-30 17:33:22 +0900

    feat : SecondViewController setup - 프로그래밍적 접근
    + UILabel 객체 생성 및 속성 추가

--- iOS 개발 실전(swift)/UI 커스텀(프로그래밍적 접근)/TabBarController/TabBarController/ViewController/SecondViewController.swift
+++ iOS 개발 실전(swift)/UI 커스텀(프로그래밍적 접근)/TabBarController/TabBarController/ViewController/SecondViewController.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+        setup()
     }
     
-
+    fileprivate func setup() {
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        
+        title.text = "두번째 탭"
+        title.textColor = UIColor.red
+        title.textAlignment = .center // 레이블 내에서 중앙 정렬
+        title.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        title.sizeToFit() // 콘텐츠의 내용에 맞게 레이블 크기 변경
+        
+        title.center.x = self.view.frame.width / 2 // x 축의 중앙에 오도록
+        self.view.addSubview(title)
+    }
     /*
     // MARK: - Navigation
 
